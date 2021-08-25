89b67db1d0f5d8797e968369c196181e2db08cbf
brcodington78 <brcodington@gmail.com>
2021-08-25 10:49:33 -0700

    compress problem

--- structy.js
+++ structy.js
@@ -19,3 +19,22 @@ const uncompress = (s) => {
   
   uncompress("2c3a1t"); // -> 'ccaaat'
   
+
+  const compress = (s) => {
+    let currentL = '';
+    let count = 0;
+    let newStr = ''
+    
+    for(let i = 0; i < s.length; i++){
+      count += 1;
+      if (s[i] !== s[i + 1] && count !== 1){
+        newStr += `${count}` + s[i]
+        count = 0
+      } else if (s[i] !== s[i + 1]) {
+        newStr += s[i]
+        count = 0
+      }
+    }
+    return newStr
+  };
+  
