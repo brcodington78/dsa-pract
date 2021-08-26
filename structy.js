86e92dddf0a2dd5e16b31d3186e1291c28656d1a
brcodington78 <brcodington@gmail.com>
2021-08-26 10:08:31 -0700

    pairSum

--- structy.js
+++ structy.js
@@ -79,4 +79,14 @@ const uncompress = (s) => {
     return topChar
   };
   
+
+  const pairSum = (numbers, targetSum) => {
+    for(let i = 0; i < numbers.length; i ++)  {
+      for(let j = 1; j < numbers.length; j ++) {
+        if((numbers[i] + numbers[j]) === targetSum) {
+          return [i,j]
+        }
+      }
+    }
+  };
   
