c9a755df978474807db71bc0c2ae5e8a4f95822c
brcodington78 <brcodington@gmail.com>
2021-09-30 12:36:58 -0700

    minChange

--- structy.js
+++ structy.js
@@ -1135,3 +1135,28 @@ const sumPossible = (amount, numbers, memo = {}) => {
 
   return false;
 };
+
+const minChange = (amount, coins) => {
+  const answer = _minChange(amount, coins);
+  if (answer === Infinity) {
+    return -1;
+  } else {
+    return answer;
+  }
+};
+
+const _minChange = (amount, coins, memo = {}) => {
+  if (amount in memo) return memo[amount];
+  if (amount === 0) return 0;
+  if (amount < 0) return Infinity;
+
+  let minCoins = Infinity;
+  for (let coin of coins) {
+    const numCoins = 1 + _minChange(amount - coin, coins, memo);
+    if (numCoins < minCoins) {
+      minCoins = numCoins;
+    }
+  }
+  memo[amount] = minCoins;
+  return minCoins;
+};
