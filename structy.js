720059c939faf3ba8e5c319144de9e0119918236
brcodington78 <brcodington@gmail.com>
2021-10-13 15:48:47 -0700

    quickestConcat

--- structy.js
+++ structy.js
@@ -1397,3 +1397,29 @@ const canConcat = (s, words, currentStr = "", memo = {}) => {
   return false;
 };
 
+
+const quickestConcat = (s, words) => {
+  const answer = _quickestConcat(s, words);
+
+  if (answer === Infinity) {
+    return -1;
+  } else {
+    return answer;
+  }
+};
+
+const _quickestConcat = (s, words, memo = {}) => {
+  if (s in memo) return memo[s];
+  if (s === "") return 0;
+
+  let minWords = Infinity;
+  for (let word of words) {
+    if (s.startsWith(word)) {
+      const suffix = s.slice(word.length);
+      const attempt = 1 + _quickestConcat(suffix, words, memo);
+      minWords = Math.min(minWords, attempt);
+    }
+  }
+  memo[s] = minWords;
+  return memo[s];
+};
